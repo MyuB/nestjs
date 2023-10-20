90607ae2d9553eecbb2c5ed6a51b81ed62386d32
이민재/연구/CCS서버개발팀 <minjae.lee98@hyundai.com>
2023-10-20 13:59:40 +0900

    project init

---
+++ src/tasks/tasks.service.ts
@@ -0,0 +1,66 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { Task, TaskStatus } from './task.model';
+import { v4 as uuid } from 'uuid';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { GetTasksFilterDto } from './dto/get-task-filter.dto';
+
+@Injectable()
+export class TasksService {
+  private tasks: Task[] = [];
+
+  public getAllTasks(): Task[] {
+    return this.tasks;
+  }
+
+  public createTasks(createTaskDto: CreateTaskDto): Task {
+    const { title, description } = createTaskDto;
+
+    const task: Task = {
+      id: uuid(),
+      title,
+      description,
+      status: TaskStatus.OPEN,
+    };
+    this.tasks.push(task);
+
+    return task;
+  }
+
+  public getTaskById(id: string): Task {
+    const found = this.tasks.find((task) => task.id === id);
+    if (!found) {
+      throw new NotFoundException();
+    }
+    return found;
+  }
+
+  public deleteTaskById(id: string): void {
+    const found = this.getTaskById(id);
+    this.tasks = this.tasks.filter((task) => task.id !== found.id);
+  }
+
+  public updateStatusById(id: string, status: TaskStatus): Task {
+    const task = this.getTaskById(id);
+    task.status = status;
+    return task;
+  }
+
+  public getTasksWithFilters(filterDto: GetTasksFilterDto) {
+    const { status, search } = filterDto;
+
+    let tasks = this.getAllTasks();
+
+    if (status) {
+      tasks = this.tasks.filter((task) => task.status === status);
+    }
+    if (search) {
+      tasks = this.tasks.filter((task) => {
+        if (task.title.includes(search) || task.description.includes(search))
+          return true;
+        return false;
+      });
+    }
+
+    return tasks;
+  }
+}
